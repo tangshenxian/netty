5ff89a7e35e1132cc8d70b0d38e263ea22a39cff
tangshenxian <1125584876@qq.com>
2022-05-13 15:51:20 +0800

    chat login

--- src/main/java/com/shenxian/netty/client/ChatClient.java
+++ src/main/java/com/shenxian/netty/client/ChatClient.java
@@ -1,16 +1,21 @@
 package com.shenxian.netty.client;
 
+import com.shenxian.netty.message.LoginRequestMessage;
 import com.shenxian.netty.protocol.MessageCodecSharable;
 import com.shenxian.netty.protocol.ProtocolFrameDecoder;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author: shenxian
@@ -33,6 +38,37 @@ public class ChatClient {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(loggingHandler);
                     ch.pipeline().addLast(messageCodec);
+                    ch.pipeline().addLast("client handler", new ChannelInboundHandlerAdapter() {
+                        // 连接建立后触发active事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            // 连接建立后接受用户在控制台的输入
+                            new Thread(() -> {
+                                Scanner scanner = new Scanner(System.in);
+                                System.out.println("请输入用户名：");
+                                String username = scanner.nextLine();
+                                System.out.println("请输入密码：");
+                                String password = scanner.nextLine();
+                                // 构造登录的消息对象
+                                LoginRequestMessage message = new LoginRequestMessage(username, password);
+                                // 发送登录消息
+                                ctx.writeAndFlush(message);
+
+                                System.out.println("等待后续操作。。。");
+                                try {
+                                    System.in.read();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }, "system in").start();
+                        }
+
+                        // 接受服务器响应
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.debug("msg: {}", msg);
+                        }
+                    });
                 }
             });
             Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
