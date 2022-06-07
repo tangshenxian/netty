fdf0bbe2260b4e8971db7e4ac09154605e4186cc
tangshenxian <1125584876@qq.com>
2022-06-07 16:22:26 +0800

    handler

--- src/main/java/com/shenxian/netty/server/handler/GroupMembersRequestMessageHandler.java
+++ src/main/java/com/shenxian/netty/server/handler/GroupMembersRequestMessageHandler.java
@@ -1,6 +1,8 @@
 package com.shenxian.netty.server.handler;
 
 import com.shenxian.netty.message.GroupMembersRequestMessage;
+import com.shenxian.netty.message.GroupMembersResponseMessage;
+import com.shenxian.netty.server.session.GroupSessionFactory;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -12,7 +14,7 @@ import io.netty.channel.SimpleChannelInboundHandler;
 @ChannelHandler.Sharable
 public class GroupMembersRequestMessageHandler extends SimpleChannelInboundHandler<GroupMembersRequestMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, GroupMembersRequestMessage groupMembersRequestMessage) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, GroupMembersRequestMessage message) throws Exception {
+        ctx.writeAndFlush(new GroupMembersResponseMessage(GroupSessionFactory.getGroupSession().getMembers(message.getGroupName())));
     }
 }
--- src/main/java/com/shenxian/netty/server/handler/GroupQuitRequestMessageHandler.java
+++ src/main/java/com/shenxian/netty/server/handler/GroupQuitRequestMessageHandler.java
@@ -1,6 +1,9 @@
 package com.shenxian.netty.server.handler;
 
 import com.shenxian.netty.message.GroupQuitRequestMessage;
+import com.shenxian.netty.message.GroupQuitResponseMessage;
+import com.shenxian.netty.server.session.Group;
+import com.shenxian.netty.server.session.GroupSessionFactory;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -12,7 +15,12 @@ import io.netty.channel.SimpleChannelInboundHandler;
 @ChannelHandler.Sharable
 public class GroupQuitRequestMessageHandler extends SimpleChannelInboundHandler<GroupQuitRequestMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, GroupQuitRequestMessage groupQuitRequestMessage) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, GroupQuitRequestMessage message) throws Exception {
+        Group group = GroupSessionFactory.getGroupSession().removeMember(message.getGroupName(), message.getUsername());
+        if (group != null) {
+            ctx.writeAndFlush(new GroupQuitResponseMessage(true, group.getName()) + "退出成功");
+        } else {
+            ctx.writeAndFlush(new GroupQuitResponseMessage(false, message.getGroupName() + "群聊不存在"));
+        }
     }
 }
