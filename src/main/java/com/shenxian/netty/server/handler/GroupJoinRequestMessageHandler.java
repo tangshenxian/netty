bda143f5a1f2e9966461d130d630f613d7b0eccb
shenxian <123!Abcv>
2022-06-07 16:22:26 +0800

    handler

--- src/main/java/com/shenxian/netty/server/handler/GroupJoinRequestMessageHandler.java
+++ src/main/java/com/shenxian/netty/server/handler/GroupJoinRequestMessageHandler.java
@@ -1,6 +1,9 @@
 package com.shenxian.netty.server.handler;
 
 import com.shenxian.netty.message.GroupJoinRequestMessage;
+import com.shenxian.netty.message.GroupJoinResponseMessage;
+import com.shenxian.netty.server.session.Group;
+import com.shenxian.netty.server.session.GroupSessionFactory;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -12,7 +15,12 @@ import io.netty.channel.SimpleChannelInboundHandler;
 @ChannelHandler.Sharable
 public class GroupJoinRequestMessageHandler extends SimpleChannelInboundHandler<GroupJoinRequestMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, GroupJoinRequestMessage groupJoinRequestMessage) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, GroupJoinRequestMessage message) throws Exception {
+        Group group = GroupSessionFactory.getGroupSession().joinMember(message.getGroupName(), message.getUsername());
+        if (group != null) {
+            ctx.writeAndFlush(new GroupJoinResponseMessage(true, message.getGroupName() + "群加入成功"));
+        } else {
+            ctx.writeAndFlush(new GroupJoinResponseMessage(true, message.getGroupName() + "群不存在"));
+        }
     }
 }
